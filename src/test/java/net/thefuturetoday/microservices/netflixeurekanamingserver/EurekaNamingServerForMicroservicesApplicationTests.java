33911605272e824722c8e04665561db7cbc9e3aa
Peter Richards <peter.richards@thefuturetoday.net>
2020-09-24 19:27:34 +0100

    Initial Spring Initializr commit

---
+++ src/test/java/net/thefuturetoday/microservices/netflixeurekanamingserver/EurekaNamingServerForMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package net.thefuturetoday.microservices.netflixeurekanamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaNamingServerForMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
