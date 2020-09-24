ded5d3f370f5516ba50b5a5f8abfd786120aefeb
Peter Richards <peter.richards@thefuturetoday.net>
2020-09-24 19:30:07 +0100

    Setup Eureka server

--- src/main/java/net/thefuturetoday/microservices/netflixeurekanamingserver/EurekaNamingServerForMicroservicesApplication.java
+++ src/main/java/net/thefuturetoday/microservices/netflixeurekanamingserver/EurekaNamingServerForMicroservicesApplication.java
@@ -2,8 +2,10 @@ package net.thefuturetoday.microservices.netflixeurekanamingserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaNamingServerForMicroservicesApplication {
 
 	public static void main(String[] args) {
